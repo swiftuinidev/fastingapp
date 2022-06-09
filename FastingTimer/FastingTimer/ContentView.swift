2dfc4c028ca39f30db6e75753c49e5b4989b62bc
David Buckley <swiftuinidev@gmail.com>
2022-06-09 22:48:22 +0100

    Making the progress ring ;)

--- FastingTimer/FastingTimer/ContentView.swift
+++ FastingTimer/FastingTimer/ContentView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    let date = Date()
+    let dateFormatter = DateFormatter()
+
     var body: some View {
         ZStack{
          Color(#colorLiteral(red: 0.06426735233, green: 0.007159079822, blue: 0.07578741644, alpha: 0.8470588235))
                 .ignoresSafeArea()
-            
+            content
         }
             
         
@@ -20,10 +23,68 @@ struct ContentView: View {
     
     var content : some View
     {
-        VStack{
-            //Mark: Progress Ring View
-         }
-    }
+        
+        ZStack {
+            VStack(spacing:40){
+                //MARK; Lets get started
+                Text("Lets get started")
+                    .font(.headline)
+                    .foregroundColor(.blue)
+                
+                
+                //MARK: Fasting Plan
+                
+                Text("16:8")
+                    .fontWeight(.bold)
+                    .padding(.horizontal,24)
+                    .padding(.vertical,8)
+                    .background(.thinMaterial)
+                    .cornerRadius(20)
+                
+                Spacer()
+               
+            }
+            .padding()
+            VStack(spacing:40){
+                
+            //Mark: Progress Ring
+                ProgressRing()
+                
+                HStack(spacing:60){
+                    //MARK: Start Time
+                    VStack(spacing:5){
+                        Text("Start")
+                            .opacity(0.7)
+                        
+                        Text(Date(),format: .dateTime.weekday().hour().minute().second())
+                            .fontWeight(.bold)
+                    }
+                    
+                    //MARK: Start Time
+                    VStack(spacing:5){
+                        Text("End")
+                            .opacity(0.7)
+                        
+                        Text(Date().addingTimeInterval(16),format: .dateTime.weekday().hour().minute().second())
+                            .fontWeight(.bold)
+                    }
+                }
+                Button{
+                    
+                    
+                }label:{
+                    Text("Start Fasting")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.horizontal,24)
+                        .padding(.vertical,8)
+                        .background(.thinMaterial)
+                        .cornerRadius(20)
+                }
+               
+            }
+            .foregroundColor(.white)
+        }    }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- FastingTimer/FastingTimer/ProgressRing.swift
+++ FastingTimer/FastingTimer/ProgressRing.swift
@@ -8,23 +8,54 @@
 import SwiftUI
 
 struct ProgressRing: View {
-    @State var progress = 0.5
+    @State var progress = 0.0
     let colors: [Color] = [.yellow, .red,.blue, .purple]
 
     var body: some View {
         ZStack{
-            // Mark:Place Holder Ring
+            // MARK:Place Holder Ring
             Circle()
                 .stroke(lineWidth: 20)
                 .foregroundColor(.gray)
                 .opacity(0.1)
             
-            // Mark: Colored Ring
+            // MARK: Colored Ring
             Circle()
                 .trim(from: 0.0, to: min(progress,1.0))
                 .stroke(  AngularGradient(gradient: Gradient(colors: colors), center: .center,startAngle: .degrees(0), endAngle: .degrees(360 + 45)) ,style: StrokeStyle(lineWidth: 15, lineCap: .round, lineJoin: .round))
                 .rotationEffect((Angle(degrees: 270)))
-
+                .animation(.easeInOut(duration: 1.0),value:progress)
+                .onAppear{
+                    progress=1
+                }
+            
+            VStack(spacing:30)
+            {
+                //MARK : Elapsed Time
+                
+                VStack(spacing:5)
+                {
+                    Text("Elapsed Time")
+                        .opacity(0.7)
+                    
+                    Text("0:00")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+                .padding(.top)
+                
+                //MARK :Remiaing Time
+                
+                VStack(spacing:5)
+                {
+                    Text("Remaining Time")
+                        .opacity(0.7)
+                    
+                    Text("0:00")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                }
+            }
         }
         .frame(width: 250, height: 250)
         .padding()
