1f8d5859b21cfa01b4902fd84aa29b84ee8a2944
David Buckley <swiftuinidev@gmail.com>
2022-06-12 23:17:32 +0100

    Updated code

---
+++ OneHealth/OneHelath/OneHelath/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  OneHelath
+//
+//  Created by David Buckley on 11/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            
+            Text("Calroie Count")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OneHealth/OneHelath/OneHelath/OneHelathApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneHelathApp.swift
+//  OneHelath
+//
+//  Created by David Buckley on 11/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct OneHelathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
